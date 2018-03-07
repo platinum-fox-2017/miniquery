b4b85de7639784b5d7f16eaf75e9b285d75ea473
dennish8 <d3nn1sdennis@gmail.com>
2018-03-07 22:25:54 +0700

    Release 1: DOM manipulation

--- library/miniquery.js
+++ library/miniquery.js
@@ -3,3 +3,18 @@ class SweetSelector{
         return document.querySelector(input);
     }
 }
+
+class DOM{
+    static hide(input){
+        document.querySelector(input).style.visibility ="hidden";
+    }
+    static show(input){
+        document.querySelector(input).style.visibility ="visible";
+    }
+    static addClass(input,className){
+        return document.querySelector(input).classList.add(className);
+    }
+    static removeClass(input,className){
+        return document.querySelector(input).classList.remove(className);
+    }
+}
