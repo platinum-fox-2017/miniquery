cd898c91786cfc50c9f69cd1bb391c0c153b88f8
Reza Pramudhika <rezapramudhika@gmail.com>
2018-03-07 22:49:07 +0700

    release 1

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log(SweetSelector.select('#eyed'));
+console.log(SweetSelector.select('.klass'));
+console.log(SweetSelector.select('a'));
+DOM.hide('#eyed');
+DOM.show('#eyed');
+DOM.hide('.klass');
+DOM.show('.klass');
+DOM.hide('a');
+DOM.show('a');
+DOM.addClass('.klass', 'shadi')
+DOM.removeClass('.klass', 'shadi')
