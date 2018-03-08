4042063dbdc8d00dace8ba91e765efd45a25e9e2
dennish8 <d3nn1sdennis@gmail.com>
2018-03-08 09:19:39 +0700

    Release 3: Ajax

--- library/miniquery.js
+++ library/miniquery.js
@@ -23,3 +23,27 @@ class EventDispatcher{
         document.querySelector(input).addEventListener(event, cb())
     }
 }
+// class AjaxWrapper{
+//     static request (obj) {
+//         let req = new XMLHttpRequest();
+//         req.open(obj.type, obj.url, true);
+//         req.onload = obj.success
+//         req.onerror = obj.fail
+//     }
+// }
+
+class AjaxWrapper{
+    static request(obj){
+        let xhr = new XMLHttpRequest();
+        xhr.open(obj.type,obj.url,true);
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4){
+                obj.success(this.responseText);
+            } else {
+                obj.fail();
+            }
+        }
+        xhr.send(null);
+    }
+}
+
